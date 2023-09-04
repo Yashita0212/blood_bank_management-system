d3dbb1c8ecfedb57de4bf62477ee134e1373db8f
Yashita0212 <yashitadhiman2@gmail.com>
2023-09-04 19:41:03 +0530

    first

---
+++ virtual/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!e:\2022 training infosys\bloodbanksystem-django\virtual\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
